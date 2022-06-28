44ffe223c7d2b46d80e0c19d5c990c780117b08a
Ana <aceyunus@gmail.com>
2022-06-28 19:46:52 +0700

    collapse

--- application/views/project/v_lap_process.php
+++ application/views/project/v_lap_process.php
@@ -5,69 +5,53 @@
         <div class="widget-body">
           <div class="form-group row">
             <label class="col-sm-2 col-form-label">Nama Proyek</label>
-            <div class="col-sm-10">
+            <div class="col-sm-4">
               <label class="col-form-label"><?= $project['project_name'] ?></label>
             </div>
-          </div>
-          <div class="form-group row">
-            <label class="col-sm-2 col-form-label">Deskripsi Proyek</label>
-            <div class="col-sm-10">
-              <label class="col-form-label"><?= $project['description'] ?></label>
-            </div>
-          </div>
-          <div class="form-group row">
-            <label class="col-sm-2 col-form-label">Tanggal Mulai</label>
-            <div class="col-sm-10">
-              <label class="col-form-label"><?= substr($project['start_date'], 0, 10) ?></label>
-            </div>
-          </div>
-          <div class="form-group row">
-            <label class="col-sm-2 col-form-label">Tanggal Selsai</label>
-            <div class="col-sm-10">
-              <label class="col-form-label"><?= substr($project['end_date'], 0, 10) ?></label>
-            </div>
-          </div>
-          <div class="form-group row">
-            <label class="col-sm-2 col-form-label">Instansi</label>
-            <div class="col-sm-10">
-              <label class="col-form-label"><?= $project['instance'] ?></label>
-            </div>
-          </div>
-          <div class="form-group row">
-            <label class="col-sm-2 col-form-label">Tipe</label>
-            <div class="col-sm-10">
-              <label class="col-form-label"><?= $project['type'] ?></label>
-            </div>
-          </div>
-          <div class="form-group row">
             <label class="col-sm-2 col-form-label">Pembimbing</label>
-            <div class="col-sm-10">
+            <div class="col-sm-4">
               <label class="col-form-label"><?= $project['mentor_name'] ?></label>
             </div>
           </div>
           <div class="form-group row">
+            <label class="col-sm-2 col-form-label">Deskripsi Proyek</label>
+            <div class="col-sm-4">
+              <label class="col-form-label"><?= $project['description'] ?></label>
+            </div>
             <label class="col-sm-2 col-form-label">Lampiran</label>
-            <div class="col-sm-10">
+            <div class="col-sm-4">
               <label class="col-form-label">
                 <a target="_blank" href="<?= site_url('project/dwnld/project/' . $project['attachment']) ?>"><?= $project['attachment'] ?></a>
               </label>
             </div>
           </div>
           <div class="form-group row">
+            <label class="col-sm-2 col-form-label">Tanggal Mulai</label>
+            <div class="col-sm-4">
+              <label class="col-form-label"><?= substr($project['start_date'], 0, 10) ?></label>
+            </div>
             <label class="col-sm-2 col-form-label">Status</label>
-            <div class="col-sm-10">
+            <div class="col-sm-4">
               <label class="col-form-label"><?= $project['stat'] ?></label>
             </div>
           </div>
           <div class="form-group row">
+            <label class="col-sm-2 col-form-label">Tanggal Selesai</label>
+            <div class="col-sm-4">
+              <label class="col-form-label"><?= substr($project['end_date'], 0, 10) ?></label>
+            </div>
             <label class="col-sm-2 col-form-label">Progres</label>
-            <div class="col-sm-10">
+            <div class="col-sm-4">
               <label class="col-form-label"><?= $project['progress'] ?> %</label>
             </div>
           </div>
           <div class="form-group row">
+            <label class="col-sm-2 col-form-label">Instansi</label>
+            <div class="col-sm-4">
+              <label class="col-form-label"><?= $project['instance'] ?></label>
+            </div>
             <label class="col-sm-2 col-form-label">Murid</label>
-            <div class="col-sm-10">
+            <div class="col-sm-4">
               <label class="col-form-label">
                 <?php foreach ($member as $k => $v) { ?>
                   <li><?= $v['member_name'] ?></li>
@@ -76,8 +60,12 @@
             </div>
           </div>
           <div class="form-group row">
+            <label class="col-sm-2 col-form-label">Tipe</label>
+            <div class="col-sm-4">
+              <label class="col-form-label"><?= $project['type'] ?></label>
+            </div>
             <label class="col-sm-2 col-form-label">Hasil Akhir</label>
-            <div class="col-sm-10">
+            <div class="col-sm-4">
               <?php if ($project['status'] == 'P') { ?>
                 <input type="file" class="form-control" name="fnl" required>
               <?php } else { ?>
--- application/views/project/v_lap_view.php
+++ application/views/project/v_lap_view.php
@@ -4,69 +4,53 @@
       <div class="widget-body">
         <div class="form-group row">
           <label class="col-sm-2 col-form-label">Nama Proyek</label>
-          <div class="col-sm-10">
+          <div class="col-sm-4">
             <label class="col-form-label"><?= $project['project_name'] ?></label>
           </div>
-        </div>
-        <div class="form-group row">
-          <label class="col-sm-2 col-form-label">Deskripsi Proyek</label>
-          <div class="col-sm-10">
-            <label class="col-form-label"><?= $project['description'] ?></label>
-          </div>
-        </div>
-        <div class="form-group row">
-          <label class="col-sm-2 col-form-label">Tanggal Mulai</label>
-          <div class="col-sm-10">
-            <label class="col-form-label"><?= substr($project['start_date'], 0, 10) ?></label>
-          </div>
-        </div>
-        <div class="form-group row">
-          <label class="col-sm-2 col-form-label">Tanggal Selsai</label>
-          <div class="col-sm-10">
-            <label class="col-form-label"><?= substr($project['end_date'], 0, 10) ?></label>
-          </div>
-        </div>
-        <div class="form-group row">
-          <label class="col-sm-2 col-form-label">Instansi</label>
-          <div class="col-sm-10">
-            <label class="col-form-label"><?= $project['instance'] ?></label>
-          </div>
-        </div>
-        <div class="form-group row">
-          <label class="col-sm-2 col-form-label">Tipe</label>
-          <div class="col-sm-10">
-            <label class="col-form-label"><?= $project['type'] ?></label>
-          </div>
-        </div>
-        <div class="form-group row">
           <label class="col-sm-2 col-form-label">Pembimbing</label>
-          <div class="col-sm-10">
+          <div class="col-sm-4">
             <label class="col-form-label"><?= $project['mentor_name'] ?></label>
           </div>
         </div>
         <div class="form-group row">
+          <label class="col-sm-2 col-form-label">Deskripsi Proyek</label>
+          <div class="col-sm-4">
+            <label class="col-form-label"><?= $project['description'] ?></label>
+          </div>
           <label class="col-sm-2 col-form-label">Lampiran</label>
-          <div class="col-sm-10">
+          <div class="col-sm-4">
             <label class="col-form-label">
               <a target="_blank" href="<?= site_url('project/dwnld/project/' . $project['attachment']) ?>"><?= $project['attachment'] ?></a>
             </label>
           </div>
         </div>
         <div class="form-group row">
+          <label class="col-sm-2 col-form-label">Tanggal Mulai</label>
+          <div class="col-sm-4">
+            <label class="col-form-label"><?= substr($project['start_date'], 0, 10) ?></label>
+          </div>
           <label class="col-sm-2 col-form-label">Status</label>
-          <div class="col-sm-10">
+          <div class="col-sm-4">
             <label class="col-form-label"><?= $project['stat'] ?></label>
           </div>
         </div>
         <div class="form-group row">
+          <label class="col-sm-2 col-form-label">Tanggal Selsai</label>
+          <div class="col-sm-4">
+            <label class="col-form-label"><?= substr($project['end_date'], 0, 10) ?></label>
+          </div>
           <label class="col-sm-2 col-form-label">Progres</label>
-          <div class="col-sm-10">
+          <div class="col-sm-4">
             <label class="col-form-label"><?= $project['progress'] ?> %</label>
           </div>
         </div>
         <div class="form-group row">
+          <label class="col-sm-2 col-form-label">Instansi</label>
+          <div class="col-sm-4">
+            <label class="col-form-label"><?= $project['instance'] ?></label>
+          </div>
           <label class="col-sm-2 col-form-label">Murid</label>
-          <div class="col-sm-10">
+          <div class="col-sm-4">
             <label class="col-form-label">
               <?php foreach ($member as $k => $v) { ?>
                 <li><?= $v['member_name'] ?></li>
@@ -75,8 +59,12 @@
           </div>
         </div>
         <div class="form-group row">
+          <label class="col-sm-2 col-form-label">Tipe</label>
+          <div class="col-sm-4">
+            <label class="col-form-label"><?= $project['type'] ?></label>
+          </div>
           <label class="col-sm-2 col-form-label">Hasil Akhir</label>
-          <div class="col-sm-10">
+          <div class="col-sm-4">
             <label class="col-form-label"> <a target="_blank" href="<?= site_url('project/dwnld/project/' . $project['final_att']) ?>"><?= $project['final_att'] ?></a></label>
           </div>
         </div>
